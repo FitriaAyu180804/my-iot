692a318c110c0ab59e37a3a37f426bb98a0cd15b
Fitria <fitria180804@gmail.com>
2024-05-11 10:10:38 +0700

    link the navbar menu

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,23 +13,23 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-success">
         <div class="container">
-          <a class="navbar-brand" href="#">my-iot</a>
+          <a class="navbar-brand" href="/dashboard">my-iot</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                <a class="nav-link active" aria-current="page" href="/dashboard">Dashboard</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Devices</a>
+                <a class="nav-link" href="/devices">Devices</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Rules</a>
+                <a class="nav-link" href="/rules">Rules</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link">Users</a>
+                <a class="nav-link" href="/users">Users</a>
               </li>
             </ul>
           </div>
