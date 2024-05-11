2e9f453a4beb4e846081e24929602556b56b5bf9
Fitria <fitria180804@gmail.com>
2024-05-11 10:36:49 +0700

    apply extending layout to all view

--- resources/views/devices.blade.php
+++ resources/views/devices.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-</head>
-<body>
+@extends('layouts.main')
+@section('container')
     <h1>Devices</h1>
 
     <h2>Device Name: {{ $name }}</h2>
     <h3>Current Value: {{ $current_value }} (Rang: {{ $min_value }} - {{ $max_value}})</h3>
-</body>
-</html>
+@endsection
+
