6a85771cf48f77e64df581db020baf166afecfd7
Fitria <fitria180804@gmail.com>
2024-05-11 09:35:21 +0700

    create views for dashboard, devices, rules, user and edit the routes/web..php

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+
+// dashboard, devices, rules, users
+
+Route::get('/dashboard', function(){
+    return view('dashboard');
+});
+
+Route::get('/devices', function(){
+    return view('devices');
+});
+
+Route::get('/rules', function(){
+    return view('rules');
+});
+
+Route::get('/users', function(){
+    return view('users');
 });
